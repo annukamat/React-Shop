93ff0a402f5eec00f5ebbf2380ea3f753029fbb1
annukamat <43543343+annukamat@users.noreply.github.com>
2020-05-27 10:52:36 +0530

    Update http-service.js

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -3,7 +3,7 @@ import 'whatwg-fetch';
 class HttpService {
   getProducts = () => {
     var promise = new Promise((resolve, reject) => {
-      fetch('http://localhost:3000/product').then((response) => {
+      fetch('https://swag-shop-ak.herokuapp.com/product').then((response) => {
         resolve(response.json());
       });
     });
